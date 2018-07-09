a82741fbd86f6358d047c90235125397b34a89a5
Thomas <Thomas@PC_Assacre>
2018-07-09 19:22:50 +0200

    depot 2 09 19H15

---
+++ TronGit/src/test/java/TronMaven/TronGit/testScene.java
@@ -0,0 +1,26 @@
+package TronMaven.TronGit;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class testScene {
+	
+	@Test
+	public void testDivisionPar2() {
+		int a =20;
+		Scene scene = new Scene();
+		int b = scene.div(a);
+		assertEquals(10, b);	
+	}
+	
+	@Test
+	public void testMultiplicationPar2() {
+		int a =250;
+		Scene scene = new Scene();
+		int b = scene.mul(a);
+		assertEquals(500, b);	
+	}
+
+
+}
